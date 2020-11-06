7db541893fc5a96f5b58bd3cf04013b0d76a5027
Cameron Lay <cameronlay15@gmail.com>
2020-11-05 19:46:21 -0500

    server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+app.use(routes);
+
+const MongoOpts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearch", MongoOpts);
+
+app.listen(PORT, function() {
+  console.log(`Server running on${PORT}!`);
+});
